f79a6f1f298f5569f8a076a1cf7333913e720e22
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-06-28 23:30:34 +0500

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 const port = process.env.PORT;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
