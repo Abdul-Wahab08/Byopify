b1c8fbd9a48536e6467544a507c52f03f5ce867d
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-07-11 18:18:58 +0500

    feat: define type for redux state

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type EnhancedStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 
-export const store = configureStore({
+export const store: EnhancedStore = configureStore({
     reducer: {
         auth: authReducer,
     },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
